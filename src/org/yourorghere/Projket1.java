df7e761ee5fa48a18c90d6efb4075a6dee605dbb
student <student@240-05.INFORMATYKA.local>
2016-10-06 11:00:20 +0200

    zad2

--- src/org/yourorghere/Projket1.java
+++ src/org/yourorghere/Projket1.java
@@ -99,10 +99,10 @@ public class Projket1 implements GLEventListener {
             gl.glVertex3f( 0.0f,-1.0f, -6.0f);
             gl.glEnd();
        gl.glBegin(GL.GL_QUADS);
-            gl.glVertex3f(2.0f, 3.0f, -6.0f);
-            gl.glVertex3f(2.0f,-3.0f, -6.0f);
-            gl.glVertex3f( 5.0f,-3.0f, -6.0f);
-            gl.glVertex3f( 5.0f,3.0f, -6.0f);
+            gl.glVertex3f(2.0f, 3.0f, -5.0f);
+            gl.glVertex3f(2.0f,-3.0f, -5.0f);
+            gl.glVertex3f( 5.0f,-3.0f, -5.0f);
+            gl.glVertex3f( 5.0f,3.0f, -5.0f);
             gl.glEnd();
         // Flush all drawing operations to the graphics card
         gl.glFlush();
