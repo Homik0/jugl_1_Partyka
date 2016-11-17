2a8c5abace9afda3feb22d32f779b4cf3eba82af
student <student@240-08.INFORMATYKA.local>
2016-11-17 10:32:50 +0100

    tekstury poczatek

--- src/org/yourorghere/Projket1.java
+++ src/org/yourorghere/Projket1.java
@@ -11,6 +11,13 @@ import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLCanvas;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
+import com.sun.opengl.util.texture.Texture;
+import com.sun.opengl.util.texture.TextureIO;
+import java.io.File;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+
 
 
 
@@ -24,15 +31,21 @@ public class Projket1 implements GLEventListener {
     
     //statyczne pola okreœlaj¹ce rotacjê wokó³ osi X i Y
     static Koparka koparka;
+    
     private static float xrot = 0.0f, yrot = 0.0f;
+    
     public static float ambientLight[] = { 0.3f, 0.3f, 0.3f, 1.0f };//swiat³o otaczajšce
     public static float diffuseLight[] = { 0.7f, 0.7f, 0.7f, 1.0f };//?wiat³o rozproszone
     public static float specular[] = { 1.0f, 1.0f, 1.0f, 1.0f}; //?wiat³o odbite
     public static float lightPos[] = { 0.0f, 5.0f, 5.0f, 1.0f };//pozycja ?wiat³a
+    
     public static float ambientLight1[] = { 0.3f, 0.3f, 0.3f, 1.0f };//swiat³o otaczajšce
     public static float diffuseLight1[] = { 0.7f, 0.7f, 0.7f, 1.0f };//?wiat³o rozproszone
     public static float specular1[] = { 1.0f, 1.0f, 1.0f, 1.0f}; //?wiat³o odbite
     public static float lightPos1[] = { 0.0f, 5.0f, 5.0f, 1.0f };//pozycja ?wiat³a
+    
+    static BufferedImage image1 = null,image2 = null;
+    static Texture t1 = null, t2 = null;
     public static void main(String[] args) {
         Frame frame = new Frame("Simple JOGL Application");
         GLCanvas canvas = new GLCanvas();
@@ -151,6 +164,26 @@ public class Projket1 implements GLEventListener {
         // Setup the drawing area and shading mode
         gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
+        try
+        {
+            image1 = ImageIO.read(getClass().getResourceAsStream("/pokemon.jpg"));
+            image2 = ImageIO.read(getClass().getResourceAsStream("/android.jpg"));
+        }
+        catch(Exception exc)
+        {   
+            JOptionPane.showMessageDialog(null, exc.toString());
+            return;
+        }
+
+        t1 = TextureIO.newTexture(image1, false);
+        t2 = TextureIO.newTexture(image2, false);
+        gl.glTexEnvi(GL.GL_TEXTURE_ENV, GL.GL_TEXTURE_ENV_MODE,
+        GL.GL_BLEND | GL.GL_MODULATE);
+        gl.glEnable(GL.GL_TEXTURE_2D);
+        gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_S, GL.GL_REPEAT);
+        gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_WRAP_T, GL.GL_REPEAT);
+        gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, GL.GL_NEAREST);
+        gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER, GL.GL_NEAREST);
     }
 
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
@@ -280,7 +313,10 @@ void drzewo(GL gl){
         gl.glLightfv(GL.GL_LIGHT1,GL.GL_POSITION,lightPos1,0); //pozycja ?wiat³a
         gl.glEnable(GL.GL_LIGHT1);
         gl.glEnable(GL.GL_COLOR_MATERIAL);
-        koparka.Rysuj(gl);
+        //Koparka
+       //koparka.Rysuj(gl);
+        
+        //drzewo
 //        for(int i=1;i<=5;i++){
 //            gl.glPushMatrix();
 //            for(int j=1;j<=5;j++){
@@ -382,53 +418,53 @@ void drzewo(GL gl){
 //        gl.glEnd();
         //szescian
         //scian przednia
-//        gl.glBegin(GL.GL_QUADS);
-//        gl.glColor3f(1.0f, 0.0f, 0.0f);
-//        gl.glVertex3f(-1.0f,-1.0f,1.0f);
-//        gl.glVertex3f(1.0f,-1.0f,1.0f);
-//        gl.glVertex3f(1.0f,-1.0f,-1.0f);
-//        gl.glVertex3f(-1.0f,-1.0f,-1.0f);
-//        gl.glEnd();
-//        //sciana prawa
-//        gl.glBegin(GL.GL_QUADS);
-//        gl.glColor3f(1.0f, 0.0f, 0.0f);
-//        gl.glVertex3f(1.0f,-1.0f,1.0f);
-//        gl.glVertex3f(1.0f,-1.0f,-1.0f);
-//        gl.glVertex3f(1.0f,1.0f,-1.0f);
-//        gl.glVertex3f(1.0f,1.0f,1.0f);
-//        gl.glEnd();
-//        //sciana tylnia
-//        gl.glBegin(GL.GL_QUADS);
-//        gl.glColor3f(1.0f, 0.0f, 0.0f);
-//        gl.glVertex3f(1.0f,-1.0f,-1.0f);
-//        gl.glVertex3f(-1.0f,-1.0f,-1.0f);
-//        gl.glVertex3f(-1.0f,1.0f,-1.0f);
-//        gl.glVertex3f(1.0f,1.0f,-1.0f);
-//        gl.glEnd();
-//        //sciana lewa
-//        gl.glBegin(GL.GL_QUADS);
-//        gl.glColor3f(1.0f, 0.0f, 0.0f);
-//        gl.glVertex3f(-1.0f,-1.0f,-1.0f);
-//        gl.glVertex3f(-1.0f,-1.0f,1.0f);
-//        gl.glVertex3f(-1.0f,1.0f,1.0f);
-//        gl.glVertex3f(-1.0f,1.0f,-1.0f);
-//        gl.glEnd();
-//        //gora
-//        gl.glBegin(GL.GL_QUADS);
-//        gl.glColor3f(1.0f, 0.0f, 0.0f);
-//        gl.glVertex3f(-1.0f,1.0f,1.0f);
-//        gl.glVertex3f(1.0f,1.0f,1.0f);
-//        gl.glVertex3f(1.0f,1.0f,-1.0f);
-//        gl.glVertex3f(-1.0f,1.0f,-1.0f);
-//        gl.glEnd();
-//        //dol
-//        gl.glBegin(GL.GL_QUADS);
-//        gl.glColor3f(1.0f, 0.0f, 0.0f);
-//        gl.glVertex3f(-1.0f,-1.0f,-1.0f);
-//        gl.glVertex3f(1.0f,-1.0f,-1.0f);
-//        gl.glVertex3f(1.0f,-1.0f,1.0f);
-//        gl.glVertex3f(-1.0f,-1.0f,1.0f);
-//        gl.glEnd();
+        gl.glBegin(GL.GL_QUADS);
+        gl.glColor3f(1.0f, 0.0f, 0.0f);
+        gl.glVertex3f(-1.0f,-1.0f,1.0f);
+        gl.glVertex3f(1.0f,-1.0f,1.0f);
+        gl.glVertex3f(1.0f,-1.0f,-1.0f);
+        gl.glVertex3f(-1.0f,-1.0f,-1.0f);
+        gl.glEnd();
+        //sciana prawa
+        gl.glBegin(GL.GL_QUADS);
+        gl.glColor3f(1.0f, 0.0f, 0.0f);
+        gl.glVertex3f(1.0f,-1.0f,1.0f);
+        gl.glVertex3f(1.0f,-1.0f,-1.0f);
+        gl.glVertex3f(1.0f,1.0f,-1.0f);
+        gl.glVertex3f(1.0f,1.0f,1.0f);
+        gl.glEnd();
+        //sciana tylnia
+        gl.glBegin(GL.GL_QUADS);
+        gl.glColor3f(1.0f, 0.0f, 0.0f);
+        gl.glVertex3f(1.0f,-1.0f,-1.0f);
+        gl.glVertex3f(-1.0f,-1.0f,-1.0f);
+        gl.glVertex3f(-1.0f,1.0f,-1.0f);
+        gl.glVertex3f(1.0f,1.0f,-1.0f);
+        gl.glEnd();
+        //sciana lewa
+        gl.glBegin(GL.GL_QUADS);
+        gl.glColor3f(1.0f, 0.0f, 0.0f);
+        gl.glVertex3f(-1.0f,-1.0f,-1.0f);
+        gl.glVertex3f(-1.0f,-1.0f,1.0f);
+        gl.glVertex3f(-1.0f,1.0f,1.0f);
+        gl.glVertex3f(-1.0f,1.0f,-1.0f);
+        gl.glEnd();
+        //gora
+        gl.glBegin(GL.GL_QUADS);
+        gl.glColor3f(1.0f, 0.0f, 0.0f);
+        gl.glVertex3f(-1.0f,1.0f,1.0f);
+        gl.glVertex3f(1.0f,1.0f,1.0f);
+        gl.glVertex3f(1.0f,1.0f,-1.0f);
+        gl.glVertex3f(-1.0f,1.0f,-1.0f);
+        gl.glEnd();
+        //dol
+        gl.glBegin(GL.GL_QUADS);
+        gl.glColor3f(1.0f, 0.0f, 0.0f);
+        gl.glVertex3f(-1.0f,-1.0f,-1.0f);
+        gl.glVertex3f(1.0f,-1.0f,-1.0f);
+        gl.glVertex3f(1.0f,-1.0f,1.0f);
+        gl.glVertex3f(-1.0f,-1.0f,1.0f);
+        gl.glEnd();
 //        
         // Flush all drawing operations to the graphics card
         gl.glFlush();
